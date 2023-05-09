a3a169eed915c8b129b8e479166f4c739b791c9f
Daniel McKnight <daniel@neongecko.com>
2023-05-09 12:56:28 -0700

    Initial implementation of Neon skill to connect to MQ service Update docs and license notices for Neon

--- README.md
+++ README.md
@@ -1,35 +1,13 @@
-# ChatGPT Persona Fallback Skill
+# <img src='./logo.svg' card_color="#FF8600" width="50" style="vertical-align:bottom" style="vertical-align:bottom">LLM Fallback  
+  
+## Summary
+Get an LLM response from the Neon Diana backend.
 
-When in doubt, ask chatgpt, powered by [OVOS Persona](https://github.com/OpenVoiceOS/ovos-solver-plugin-openai-persona)
-
-You need to configure an api_key, get it at https://beta.openai.com/account/api-keys
-
-
-## About 
-
-Capabilities:
-- Remembers what user said earlier in the conversation
-- Trained to decline inappropriate requests
-
-Limitations:
-
-- May occasionally generate incorrect information
-- May occasionally produce harmful instructions or biased content
-- Limited knowledge of world and events after 2021
-
-## Configuration
-
-Under skill settings you can tweak some parameters for chatGPT
-
-
-- `key` - your api_key to access OpenAI
-- `persona` - can be used to create a "persona", give a personality to chatGPT
-- `model` - LLM model to use, valid options `"gpt-3.5-turbo", "ada", "babbage", "curie", "text-davinci-002", "text-davinci-003"`
-
-The default persona is `helpful, creative, clever, and very friendly.`
-
+## Description
+Converse with a bot by telling Neon to "ask Chat GPT". This also provides some
+better responses when no skill is able to provide a specific response.
 
 ## Examples 
 
 * "Explain quantum computing in simple terms"
-* "Got any creative ideas for a 10 year old’s birthday?"
+* "Ask chat GPT what an LLM is"
--- __init__.py
+++ __init__.py
@@ -1,13 +1,41 @@
-from ovos_solver_openai_persona import OpenAIPersonaSolver
-from ovos_solver_openai_persona.prompts import OpenAIPersonaPromptSolver
+# NEON AI (TM) SOFTWARE, Software Development Kit & Application Framework
+# All trademark and other rights reserved by their respective owners
+# Copyright 2008-2022 Neongecko.com Inc.
+# Contributors: Daniel McKnight, Guy Daniels, Elon Gasper, Richard Leeds,
+# Regina Bloomstine, Casimiro Ferreira, Andrii Pernatii, Kirill Hrymailo
+# BSD-3 License
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE,  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 from ovos_utils import classproperty
 from ovos_utils.log import LOG
 from ovos_utils.process_utils import RuntimeRequirements
 from ovos_workshop.skills.fallback import FallbackSkill
+from neon_utils.message_utils import get_message_user
+from neon_mq_connector.utils.client_utils import send_mq_request
+from mycroft.skills.mycroft_skill.decorators import intent_file_handler
 
 
-class ChatGPTSkill(FallbackSkill):
-
+class LLMSkill(FallbackSkill):
     @classproperty
     def runtime_requirements(self):
         return RuntimeRequirements(internet_before_load=True,
@@ -21,57 +49,49 @@ class ChatGPTSkill(FallbackSkill):
                                    no_gui_fallback=True)
 
     def __init__(self):
-        super().__init__("ChatGPT")
-        self.current_q = None
-        self.current_a = None
+        super().__init__("LLM")
+        self.chat_history = dict()
+        self._default_user = "local"
 
     def initialize(self):
-        chat_engines = ["gpt-3.5-turbo"]
-        text_completions = ["ada", "babbage", "curie", "davinci",
-                            "text-davinci-002", "text-davinci-003"]
-        code_completions = ["code-cushman-001", "code-davinci-002"]
-        engine = self.settings.get("model", "gpt-3.5-turbo")
-        if engine in chat_engines:
-            self._chat = OpenAIPersonaSolver(config=self.settings)
-        elif engine in text_completions:  # davinci/ada ...
-            self._chat = OpenAIPersonaPromptSolver(config=self.settings)
-        else:
-            LOG.warning(f"valid models: {chat_engines + text_completions}")
-            raise ValueError(f"invalid OpenAI model: {engine}")
-        self.add_event("speak", self.handle_speak)
-        self.add_event("recognizer_loop:utterance", self.handle_utterance)
-        self.register_fallback(self.ask_chatgpt, 85)
+        self.register_fallback(self.fallback_llm, 85)
 
-    def handle_utterance(self, message):
-        utt = message.data.get("utterances")[0]
-        if self.current_q and self.current_a:
-            self._chat.qa_pairs.append((self.current_q, utt))
-        self.current_q = utt
-        self.current_a = None
-        # TODO: imperfect, subject to race conditions between bus messages
-        # use session_id/ident to track all matches
-
-    def handle_speak(self, message):
-        utt = message.data.get("utterance")
-        if not self.current_q:
-            # TODO - use session_id/ident to track all matches
-            # append to previous question if multi-speak answer
-            return
-        if self.current_a:
-            self.current_a += ". " + utt
-        else:
-            self.current_a = utt
-
-    def ask_chatgpt(self, message):
-        self.current_a = None
+    def fallback_llm(self, message):
         utterance = message.data['utterance']
-        answer = self._chat.get_spoken_answer(utterance)
+        user = get_message_user(message) or self._default_user
+        answer = self._get_response(utterance, user)
         if not answer:
+            LOG.info(f"No fallback response")
             return False
-        self.current_q = None
         self.speak(answer)
         return True
 
+    @intent_file_handler("ask_chatgpt.intent")
+    def handle_ask_chatgpt(self, message):
+        utterance = message.data['utterance']
+        user = get_message_user(message) or self._default_user
+        resp = self._get_response(utterance, user)
+        self.speak(resp)
+
+    def _get_response(self, query: str, user: str) -> str:
+        """
+        Get a response from an LLM
+        :param query: User utterance to generate a response to
+        :param user: Username making the request
+        :returns: Speakable response to the user's query
+        """
+        # TODO: support multiple LLM backends?
+        self.chat_history.setdefault(user, list())
+        mq_resp = send_mq_request("/llm", {"query": query,
+                                           "history": self.chat_history[user]},
+                                  "chat_gpt_input")
+        resp = mq_resp.get("response") or ""  # TODO: Get response from MQ endpoint
+        if resp:
+            self.chat_history[user].append(("user", query))
+            self.chat_history[user].append(("llm", resp))
+        LOG.debug(f"Got LLM response: {resp}")
+        return resp
+
 
 def create_skill():
-    return ChatGPTSkill()
+    return LLMSkill()
