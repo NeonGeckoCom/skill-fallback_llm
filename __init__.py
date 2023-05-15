4c7b4f2fbbcbbaf9dc5063f2f7b3f5cdeda35fa7
Daniel McKnight <daniel@neongecko.com>
2023-05-15 14:47:00 -0700

    Update email formatting Fix typo in email intent

--- __init__.py
+++ __init__.py
@@ -147,7 +147,7 @@ class LLMSkill(NeonFallbackSkill):
         history = self.chat_history.get(username)
         email_text = ""
         for entry in history:
-            formatted = entry[1].replace('\n', '\n\t')
+            formatted = entry[1].replace('\n\n', '\n\t...')
             email_text += f"[{entry[0]}] {formatted}\n"
         NeonSkill.send_email(self, "LLM Conversation", email_text,
                              email_addr=email)
